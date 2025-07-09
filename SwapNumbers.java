2e5e73e5982d8b417930b3c012f3d919d58e4874
Ritesh kumar sahoo <240301120381@centurionuniv.edu.in>
2025-07-09 20:42:07 +0530

    Update SwapNumbers.java

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,5 +1,7 @@
-public class SwapNumbers {
-    public static void main(String[] args) {
+public class SwapNumbers
+    {
+    public static void main(String[] args)
+    {
         int a = 5;
         int b = 10;
         System.out.println("Before swap: a = " + a + ", b = " + b);
