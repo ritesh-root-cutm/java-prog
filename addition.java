b687b15bac04073c7c6f917dae4481072fcadf45
Ritesh kumar sahoo <240301120381@centurionuniv.edu.in>
2025-07-09 20:50:39 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 4;
+        int sum = a + b;
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("Sum: " + sum);
+    }
+}
