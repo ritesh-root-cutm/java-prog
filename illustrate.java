9a83d3f217d69d07a114dad009374cf58d12af10
Ritesh kumar sahoo <240301120381@centurionuniv.edu.in>
2025-07-12 23:53:47 +0530

    Add files via upload

---
+++ illustrate.java
@@ -0,0 +1,16 @@
+public class illustrate {
+    public static void main(String[] args) {
+        // decimal-form literal (float type suffix 'f' or 'F' is required)
+        float a = 101.230f;
+
+        // It is a decimal literal despite the leading zero
+        float b = 0123.222f;
+
+        // Hexadecimal floating-point literals are NOT supported in Java and will cause an error
+        // float c = 0x123.222; // This line causes compilation error
+
+        System.out.println(a);
+        System.out.println(b);
+        // System.out.println(c); // Commented out due to error
+    }
+}
